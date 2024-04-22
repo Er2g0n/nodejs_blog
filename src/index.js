da131b2c5d25806eed351fca952374176ec10d15
Er2g0n <hnguyenngoc.h@gmail.com>
2024-04-23 00:21:16 +0700

    Refactor code: creatre routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname ,'public')));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine with handlebars
 app.engine('.hbs', handlebars.engine({
@@ -16,14 +24,8 @@ app.engine('.hbs', handlebars.engine({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-console.log(__dirname)
-
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//Routes init
+route(app);
 
 // Start the server
 app.listen(port, () => {
